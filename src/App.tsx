d1bb54a3e0282adcc18c087e0f118257ce4c6353
godwin366 <godwingodwin366@gmail.com>
2024-08-04 14:17:55 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createTheme } from "@mui/material";
+import "./App.css";
+import AppRouter from "./router";
+import { ThemeProvider } from "@emotion/react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
+const baseTheme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily:
+        "Work Sans, Poppins, Helvetica Neue, Helvetica, Arial, sans-serif",
+      fontSize: "14px",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <ThemeProvider theme={baseTheme}>
+        <AppRouter />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
